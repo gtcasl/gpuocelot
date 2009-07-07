fc4ce6d8a32f824631bdd0f453c870bc0d95076a
Gregory Diamos <gregory.diamos@gatech.edu>
2009-07-07 14:24:17 +0000

    ENH: Split SSA functionality into SSAGraph class.

--- ocelot/analysis/interface/SSAGraph.h
+++ ocelot/analysis/interface/SSAGraph.h
@@ -8,13 +8,45 @@
 #ifndef SSA_GRAPH_H_INCLUDED
 #define SSA_GRAPH_H_INCLUDED
 
+#include <ocelot/analysis/interface/DataflowGraph.h>
+
 namespace analysis
 {
 
 	/*!
 		\brief SSA Graph
 	*/
-	
+	class SSAGraph
+	{
+		private:
+			class Block
+			{
+				public:
+					typedef std::unordered_map< DataflowGraph::RegisterId, 
+						DataflowGraph::RegisterId > RegisterMap;
+					typedef std::unordered_map< DataflowGraph::RegisterId, 
+						DataflowGraph::Instruction > PhiMap;
+		
+				public:
+					RegisterMap regs;
+					PhiMap phi;
+			
+				public:
+					bool process();
+			};
+		
+			typedef std::unordered_map< DataflowGraph::iterator, 
+				Block > BlockMap;
+		
+		private:
+			DataflowGraph& _graph;
+			BlockMap _blocks;
+		
+		public:
+			SSAGraph( DataflowGraph& graph );
+			void toSsa();
+			void fromSsa();
+	};
 
 }
 

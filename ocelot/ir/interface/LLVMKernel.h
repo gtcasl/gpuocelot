7c5698014625fd30737f45b18ac7986c15ecfeca
Gregory Diamos <gregory.diamos@gatech.edu>
2009-09-03 23:08:43 +0000

    ENH: Changed LLVM kernel so that it inherits from ExecutableKernel.  The plan is to augment it such that it is able to invoke the LLVM JIT itself.

--- ocelot/ir/interface/LLVMKernel.h
+++ ocelot/ir/interface/LLVMKernel.h
@@ -8,7 +8,7 @@
 #ifndef LLVM_KERNEL_H_INCLUDED
 #define LLVM_KERNEL_H_INCLUDED
 
-#include <ocelot/ir/interface/Kernel.h>
+#include <ocelot/ir/interface/ExecutableKernel.h>
 #include <ocelot/ir/interface/LLVMInstruction.h>
 #include <ocelot/ir/interface/LLVMStatement.h>
 
@@ -21,7 +21,7 @@ namespace ir
 {
 
 	/*! \brief A class containing a complete representation of an LLVM kernel */
-	class LLVMKernel : public Kernel
+	class LLVMKernel : public ExecutableKernel
 	{
 		friend class translator::PTXToLLVMTranslator;
 		public:
@@ -36,18 +36,30 @@ namespace ir
 			std::string _code;
 			/*! \brief The set of statements representing the kernel */
 			LLVMStatementVector _statements;
+			/*! \brief The CTA dimensions */
+			ir::dim3 ctaDimensions;
+			/*! \brief The kernel dimensions */
+			ir::dim3 kernelDimensions;
 		
 		public:
 			/*! \brief Sets the ISA */
 			LLVMKernel();
-			/*! \brief Initializes the base class from another Kernel */
-			LLVMKernel( const Kernel& k );
+			/*! \brief Initialized the base class from a kernel and executive */
+			LLVMKernel( const Kernel& k, const executive::Executive* c = 0 );
+			
+		public:
+			/*! \brief Launch a kernel on a 2D grid */
+			void launchGrid(int width, int height);
+			/*! \brief Sets the shape of a cta in the kernel */
+			void setKernelShape(int x, int y, int z);
+			/*! \brief Is the kernel executable? yes */
+			bool executable();
 			
 		public:
 			/*! \brief Assemble the LLVM kernel from the set of statements */
 			void assemble();
 			
-			/*! \brief Is the kernel assembled */
+			/*! \brief Is the kernel assembled? */
 			bool assembled() const;
 			
 			/*! \brief Get the assembly code */

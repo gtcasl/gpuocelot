b9cb49d314d4d57698ae20fc7dd2f726e23e20eb
Gregory Diamos <solusstultus@gmail.com>
2009-11-22 18:13:01 +0000

    ENH: Refined the call latency test to include a launch-use chain
    Former-commit-id: 38d18335d4fc3a6126183b542dc63285e6af8f6c

--- tests/cuda2.3/tests/TestCallLatency/TestCallLatency.cpp
+++ tests/cuda2.3/tests/TestCallLatency/TestCallLatency.cpp
@@ -16,15 +16,35 @@ namespace test
 {
 	bool TestCallLatency::testKernelLaunchLatency()
 	{
-		cudaConfigureCall( dim3( ctas, 1, 1 ), dim3( threads, 1, 1 ), 0, 0 );
-
+		unsigned int* address;
+		
+		cudaMalloc( (void**) &address, sizeof( unsigned int ) );
+		cudaMemset( address, 0, sizeof( unsigned int ) );
+		
 		std::stringstream program;
 		program << ".version 1.4\n";
 
 		program << ".target sm_10, map_f64_to_f32\n\n";
-		program << ".entry nop( )\n";
+		program << ".entry nop( .param .u64 pointer )\n";
 		program << "{\n";
-		program << " exit;\n";
+		program << " .reg .u64 %address;\n";
+		program << " .reg .u16 %cta;\n";
+		program << " .reg .u32 %value;\n";
+		program << " .reg .u16 %thread;\n";
+		program << " .reg .pred %writer;\n";
+		program << " Entry:\n";
+		program << "  ld.param.u64 %address, [pointer];\n";
+		program << "  mov.u16 %cta, %ctaid.x;\n";
+		program << "  mov.u16 %thread, %tid.x;\n";
+		program << "  setp.eq.u16 %writer, %cta, 0;\n";
+		program << "  setp.eq.and.u16 %writer, %thread, 0, %writer;\n";
+		program << "  @!%writer bra Exit;\n";
+		program << " Store:\n";
+		program << "  ld.global.u32 %value, [%address];\n";
+		program << "  add.u32 %value, %value, 1;\n";
+		program << "  st.global.u32 [%address], %value;\n";
+		program << " Exit:\n";
+		program << "  exit;\n";
 		program << "}";
 		
 		ocelot::registerPTXModule( program, "callLatency" );
@@ -33,17 +53,31 @@ namespace test
 		
 		hydrazine::Timer timer;
 		
+		unsigned int value;
+		
 		timer.start();
-		for( unsigned int i = 0; i < iterations; ++i )
+		for( value = 0; value < iterations; )
 		{
+			cudaConfigureCall( dim3( ctas, 1, 1 ), 
+				dim3( threads, 1, 1 ), 0, 0 );
+			cudaSetupArgument( &address, sizeof( long long unsigned int ), 0 );
 			cudaLaunch( kernelPointer );
-			cudaThreadSynchronize();
+			cudaMemcpy( &value, address, sizeof( unsigned int ), 
+				cudaMemcpyDeviceToHost );
 		}
 		timer.stop();
 		
 		status << "Kernel launch latency: " 
 			<< ( timer.seconds() / iterations ) << "s\n";
 		
+		cudaFree( address );
+		
+		if( value != iterations )
+		{
+			status << " Incorrect value " << value << "\n";
+			return false;
+		}
+				
 		return true;
 	}
 
@@ -71,7 +105,7 @@ int main( int argc, char** argv )
 		"Random seed. 0 implies seed with time." );
 	parser.parse( "-v", "--verbose", test.verbose, false, 
 		"Print out info after the test has finished." );
-	parser.parse( "-i", "--iterations", test.iterations, 10, 
+	parser.parse( "-i", "--iterations", test.iterations, 100000, 
 		"The number of iterations to run." );
 	parser.parse( "-c", "--ctas", test.ctas, 16, 
 		"The number of ctas to launch." );

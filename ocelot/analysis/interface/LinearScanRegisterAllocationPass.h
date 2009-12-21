80c377d2bd7b9c99bafb8c24cf8a145ad6621c3e
Gregory Diamos <gregory.diamos@gatech.edu>
2009-12-21 18:52:33 +0000

    TRANSFER: Adding basic register allocation pass files for download at another location.  These should be off of the build path.
    Former-commit-id: 266a4be40419cfd388e1100412be867839407861

---
+++ ocelot/analysis/interface/LinearScanRegisterAllocationPass.h
@@ -0,0 +1,13 @@
+/*! \file LinearScanRegisterAllocationPass.h
+	\author Gregory Diamos <gregory.diamos@gatech.edu>
+	\date Monday December 21, 2009
+	\brief The header file for the LinearScanRegisterAllocationPass class.
+*/
+
+#ifndef LINEAR_SCAN_REGISTER_ALLOCATION_PASS_H_INCLUDED
+#define LINEAR_SCAN_REGISTER_ALLOCATION_PASS_H_INCLUDED
+
+
+
+#endif
+	

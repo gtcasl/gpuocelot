43559c03bec1f460aacfade7a8fd368c009e3133
Gregory Diamos <solusstultus@gmail.com>
2010-01-27 00:23:44 +0000

    ADD: Added thrust regression tests.
    Former-commit-id: 97e724f9daf17d85a9294a2fcb0715bb1d68bd0e

---
+++ tests/cuda2.3/tests/thrust_sort/sort.cu
@@ -0,0 +1,22 @@
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/generate.h>
+#include <thrust/sort.h>
+#include <cstdlib>
+
+int main(void)
+{
+  // generate random data on the host
+  thrust::host_vector<int> h_vec(20);
+  thrust::generate(h_vec.begin(), h_vec.end(), rand);
+
+  // transfer to device
+  thrust::device_vector<int> d_vec = h_vec;
+
+  // sort on device
+  thrust::sort(d_vec.begin(), d_vec.end());
+
+  std::cout << "TEST PASSED\n";
+
+  return 0;
+}

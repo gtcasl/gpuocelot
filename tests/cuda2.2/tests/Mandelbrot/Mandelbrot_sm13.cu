0d857978841498e21deba4d62606255be25957ae
Gregory Diamos <solusstultus@gmail.com>
2010-03-09 14:27:57 +0000

    ENH: Fixed barrier deadlock in mandelbrot
    Former-commit-id: 7768d0b5e098d65f488db8478705243cdb9197a5

--- tests/cuda2.2/tests/Mandelbrot/Mandelbrot_sm13.cu
+++ tests/cuda2.2/tests/Mandelbrot/Mandelbrot_sm13.cu
@@ -28,20 +28,17 @@ __global__ void Mandelbrot0_sm13(uchar4 *dst, const int imageW, const int imageH
     __shared__ unsigned int blockIndex;
     __shared__ unsigned int blockX, blockY;
     
-    // loop until all blocks completed
-    while(1) {
-        if ((threadIdx.x==0) && (threadIdx.y==0)) {
-            // get block to process
-            blockIndex = atomicAdd(&blockCounter, 1);
-            blockX = blockIndex % gridWidth;            // note: this is slow, but only called once per block here
-            blockY = blockIndex / gridWidth;
-        }
-#if 1        // device emu doesn't like syncthreads inside while()
-        __syncthreads();
-#endif
-  
-        if (blockIndex >= numBlocks) break;  // finish
+     if ((threadIdx.x==0) && (threadIdx.y==0)) {
+        // get block to process
+        blockIndex = atomicAdd(&blockCounter, 1);
+        blockX = blockIndex % gridWidth;            // note: this is slow, but only called once per block here
+        blockY = blockIndex / gridWidth;
+    }
+    __syncthreads();
 
+   // loop until all blocks completed
+    while(blockIndex < numBlocks) {
+ 
         // process this block
         const int ix = blockDim.x * blockX + threadIdx.x;
         const int iy = blockDim.y * blockY + threadIdx.y;
@@ -82,6 +79,16 @@ __global__ void Mandelbrot0_sm13(uchar4 *dst, const int imageW, const int imageH
 			    dst[pixel].z = (dst[pixel].z * frame + color.z + frame2) / frame1;
             }
         }
+        
+		__syncthreads();
+		if ((threadIdx.x==0) && (threadIdx.y==0)) {
+		    // get block to process
+		    blockIndex = atomicAdd(&blockCounter, 1);
+		    blockX = blockIndex % gridWidth;            // note: this is slow, but only called once per block here
+		    blockY = blockIndex / gridWidth;
+		}
+		__syncthreads();
+
 
     }
     
@@ -96,20 +103,17 @@ __global__ void MandelbrotDS0_sm13(uchar4 *dst, const int imageW, const int imag
 {
     __shared__ unsigned int blockIndex;
     __shared__ unsigned int blockX, blockY;
+
+    if ((threadIdx.x==0) && (threadIdx.y==0)) {
+        // get block to process
+        blockIndex = atomicAdd(&blockCounter, 1);
+        blockX = blockIndex % gridWidth;            // note: this is slow, but only called once per block here
+        blockY = blockIndex / gridWidth;
+    }
+    __syncthreads();
     
     // loop until all blocks completed
-    while(1) {
-        if ((threadIdx.x==0) && (threadIdx.y==0)) {
-            // get block to process
-            blockIndex = atomicAdd(&blockCounter, 1);
-            blockX = blockIndex % gridWidth;            // note: this is slow, but only called once per block here
-            blockY = blockIndex / gridWidth;
-        }
-#if 1
-        __syncthreads();
-#endif
-        
-        if (blockIndex >= numBlocks) break;  // finish
+    while(blockIndex < numBlocks) {
 
         // process this block
         const int ix = blockDim.x * blockX + threadIdx.x;
@@ -154,6 +158,14 @@ __global__ void MandelbrotDS0_sm13(uchar4 *dst, const int imageW, const int imag
 			    dst[pixel].z = (dst[pixel].z * frame + color.z + frame2) / frame1;
             }
         }
+		__syncthreads();
+		if ((threadIdx.x==0) && (threadIdx.y==0)) {
+		    // get block to process
+		    blockIndex = atomicAdd(&blockCounter, 1);
+		    blockX = blockIndex % gridWidth;            // note: this is slow, but only called once per block here
+		    blockY = blockIndex / gridWidth;
+		}
+		__syncthreads();
         
     }
 } // MandelbrotDS0

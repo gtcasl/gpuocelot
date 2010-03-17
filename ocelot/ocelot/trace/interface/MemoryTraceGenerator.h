fff5b8281ae57c4f993d046a6fc9302ecc943608
Gregory Diamos <solusstultus@gmail.com>
2010-03-17 09:46:11 +0000

    BUG: Added in destructor for memory trace generator.
    Former-commit-id: c6a24c3a0083b6a89d5d0c84a18634cbb97e7116

--- ocelot/ocelot/trace/interface/MemoryTraceGenerator.h
+++ ocelot/ocelot/trace/interface/MemoryTraceGenerator.h
@@ -182,6 +182,11 @@ namespace trace {
 		MemoryTraceGenerator();
 
 		/*!
+			Destructs the trace generator
+		*/
+		~MemoryTraceGenerator();
+
+		/*!
 			called when a traced kernel is launched to retrieve some parameters from the kernel
 		*/
 		void initialize(const ir::ExecutableKernel& kernel);

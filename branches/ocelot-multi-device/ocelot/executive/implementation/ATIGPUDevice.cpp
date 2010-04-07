44492666e703f5d79201a0e88f554c1ceffee736
Rodrigo Dominguez <rodrigo.d.dominguez@gmail.com>
2010-04-07 23:49:06 +0000

    ENH: Added ATIGPUDevice stub files
    Former-commit-id: d363d646ced4f96a2f6df5dcf46506c904dfb4fe

---
+++ branches/ocelot-multi-device/ocelot/executive/implementation/ATIGPUDevice.cpp
@@ -0,0 +1,7 @@
+/*! \file ATIGPUDevice.cpp
+	\author Rodrigo Dominguez <rdomingu@ece.neu.edu>
+	\date April 7, 2010
+	\brief The source file for the ATI GPU Device class.
+*/
+
+#include <ocelot/executive/interface/ATIGPUDevice.h>
---
+++ branches/ocelot-multi-device/ocelot/executive/interface/ATIGPUDevice.h
@@ -0,0 +1,19 @@
+/*! \file ATIGPUDevice.h
+	\author Rodrigo Dominguez <rdomingu@ece.neu.edu>
+	\date April 7, 2010
+	\brief The header file for the ATI GPU Device class.
+*/
+
+#ifndef ATIGPUDEVICE_H_INCLUDED
+#define ATIGPUDEVICE_H_INCLUDED
+
+#include <ocelot/executive/interface/Device.h>
+
+namespace executive
+{
+	class ATIGPUDevice : public Device
+	{
+	};
+}
+
+#endif

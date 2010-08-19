10634a767026b99f872dbea6d364f42df8198cfe
Gregory Diamos <solusstultus@gmail.com>
2010-08-19 07:19:02 +0000

    ADD: Added trace configuration files that I forgot when I originally checked this in.
    Former-commit-id: 63f7bf38d7977367baa27526f8e47afd3848f93c

---
+++ trace-generators/traces/implementation/TraceConfiguration.cpp
@@ -0,0 +1,214 @@
+/*! \file   TraceConfiguration.cpp
+	\author Gregory Diamos <gregory.diamos@gatech.edu>
+	\date   Saturday July 31, 2010
+	\brief  The source file for the TraceConfiguration class.
+*/
+
+#ifndef TRACE_CONFIGURATION_CPP_INCLUDED
+#define TRACE_CONFIGURATION_CPP_INCLUDED
+
+// Trace includes
+#include <traces/interface/TraceConfiguration.h>
+
+// Ocelot includes
+#include <ocelot/api/interface/ocelot.h>
+
+// Hydrazine includes
+#include <hydrazine/implementation/Exception.h>
+#include <hydrazine/implementation/debug.h>
+#include <hydrazine/implementation/json.h>
+
+#ifdef REPORT_BASE
+#undef REPORT_BASE
+#endif
+
+#define REPORT_BASE 1
+
+namespace trace
+{
+
+TraceConfiguration TraceConfiguration::Singleton;
+
+TraceConfiguration::TraceConfiguration()
+{
+	hydrazine::json::Parser parser;
+	hydrazine::json::Object* object = 0;
+
+	// initialize
+	memory = false;
+	sharedComputation = false;
+	branch = false;
+	parallelism = false;
+	instruction = false;
+	cacheSimulator = false;
+	warpSynchronous.enabled = false; 
+	performanceBound.enabled = false; 
+	convergence.enabled = false;
+
+	try
+	{
+		std::ifstream stream("configure.ocelot");
+	
+		object = parser.parse_object(stream);
+
+		hydrazine::json::Visitor mainConfig(object);
+
+		hydrazine::json::Visitor traceConfig = mainConfig["trace"];
+		if(!traceConfig.is_null())
+		{
+			database = traceConfig.parse<std::string>(
+				"database", "trace/database.trace");
+			inPlaceTraces = traceConfig.parse<bool>("inPlaceTraces", true);
+			memory = traceConfig.parse<bool>("memory", false);
+			branch = traceConfig.parse<bool>("branch", false);
+			sharedComputation = traceConfig.parse<bool>(
+				"sharedComputation", false);
+			instruction = traceConfig.parse<bool>("instruction", false);
+			parallelism = traceConfig.parse<bool>("parallelism", false);
+			cacheSimulator = traceConfig.parse<bool>("cacheSimulator", false);
+
+			// more detailed configuration for this trace generator
+			hydrazine::json::Visitor warpSyncConfig =
+				traceConfig["warpSynchronous"];
+			if(!warpSyncConfig.is_null()) 
+			{
+				warpSynchronous.enabled = warpSyncConfig.parse<bool>(
+					"enabled", false);
+				warpSynchronous.emitHotPaths = warpSyncConfig.parse<bool>(
+					"emitHotPaths", false);
+			}
+	
+			hydrazine::json::Visitor perfConfig =
+				traceConfig["performanceBound"];
+			if(!perfConfig.is_null()) 
+			{
+				performanceBound.enabled = perfConfig.parse<bool>(
+					"enabled", false);
+				std::string protocol = 
+					perfConfig.parse<std::string>("protocol", "sm_20");
+				performanceBound.protocol =
+					PerformanceBoundGenerator::Protocol_invalid;
+				if(protocol == "sm_10")
+				{
+					performanceBound.protocol =
+						PerformanceBoundGenerator::Protocol_sm_10;
+				}
+				else if(protocol == "sm_11")
+				{
+					performanceBound.protocol =
+						PerformanceBoundGenerator::Protocol_sm_11;
+				}
+				else if(protocol == "sm_12")
+				{
+					performanceBound.protocol =
+						PerformanceBoundGenerator::Protocol_sm_12;
+				}
+				else if(protocol == "sm_13")
+				{
+					performanceBound.protocol =
+						PerformanceBoundGenerator::Protocol_sm_13;
+				}
+				else if(protocol == "sm_20")
+				{
+					performanceBound.protocol =
+						PerformanceBoundGenerator::Protocol_sm_20;
+				}
+				else if(protocol == "sm_ideal")
+				{
+					performanceBound.protocol =
+						PerformanceBoundGenerator::Protocol_ideal;
+				}
+				else
+				{
+					std::cerr << "Invalid protocol '" << protocol << "'\n";
+				}
+			}
+	
+			hydrazine::json::Visitor convConfig = traceConfig["convergence"];
+			if(!convConfig.is_null())
+			{
+				convergence.enabled = convConfig.parse<bool>("enabled", false);
+				convergence.logfile = convConfig.parse<std::string>(
+					"logfile", "traces/convergence.csv");
+				convergence.dot = convConfig.parse<bool>("dot", false);
+				convergence.render = convConfig.parse<bool>("render", false);
+			}
+		}
+	}
+	catch(const hydrazine::Exception& exp) 
+	{
+		// do nothing
+	}
+
+	delete object;
+
+	if(memory)
+	{
+		report( "Creating memory trace generator" );
+		_memoryTraceGenerator.database = database;
+		_memoryTraceGenerator.headerOnly = inPlaceTraces;
+		ocelot::addTraceGenerator(_memoryTraceGenerator, true);			
+	}
+	
+	if(sharedComputation)
+	{
+		report( "Creating shared computation trace generator" );
+		_sharedComputationGenerator.database = database;
+		ocelot::addTraceGenerator(_sharedComputationGenerator, true);
+	}
+
+	if(branch)
+	{
+		report( "Creating branch trace generator" );
+		_branchTraceGenerator.database = database;
+		ocelot::addTraceGenerator(_branchTraceGenerator, true);
+	}
+
+	if(parallelism)
+	{
+		report( "Creating parallelism trace generator" );
+		_parallelismTraceGenerator.database = database;
+		ocelot::addTraceGenerator(_parallelismTraceGenerator, true);
+	}
+
+	if(instruction)
+	{
+		report( "Creating instruction trace generator" );
+		_instructionTraceGenerator.database = database;
+		ocelot::addTraceGenerator(_instructionTraceGenerator, true);
+	}
+
+	if(cacheSimulator)
+	{
+		report( "Creating cache simulator" );
+		_cacheSimulator.database = database;
+		ocelot::addTraceGenerator(_cacheSimulator, true);	
+	}
+
+	if(warpSynchronous.enabled) 
+	{
+		report( "Creating warp synchronous detector" );
+		_warpSynchronous.database = database;
+		ocelot::addTraceGenerator(_warpSynchronous, true);
+	}
+	
+	if(performanceBound.enabled) 
+	{
+		report("Creating performance bound trace generator");
+		_performanceBound.database = database;
+		_performanceBound.protocol = performanceBound.protocol;
+		ocelot::addTraceGenerator(_performanceBound, true);
+	}
+
+	if(convergence.enabled) 
+	{
+		report("Creating convergence trace generator");
+		_convergence.database = database;
+		ocelot::addTraceGenerator(_convergence, true);
+	}
+}
+
+}
+
+#endif
+
---
+++ trace-generators/traces/interface/TraceConfiguration.h
@@ -0,0 +1,122 @@
+/*! \file   TraceConfiguration.h
+	\author Gregory Diamos <gregory.diamos@gatech.edu>
+	\date   Saturday July 31, 2010
+	\brief  The header file for the TraceConfiguration class.
+*/
+
+#ifndef TRACE_CONFIGURATION_H_INCLUDED
+#define TRACE_CONFIGURATION_H_INCLUDED
+
+#include <traces/interface/WarpSynchronousGenerator.h>
+#include <traces/interface/MemoryTraceGenerator.h>
+#include <traces/interface/InstructionTraceGenerator.h>
+#include <traces/interface/ParallelismTraceGenerator.h>
+#include <traces/interface/SharedComputationGenerator.h>
+#include <traces/interface/BranchTraceGenerator.h>
+#include <traces/interface/CacheSimulator.h>
+#include <traces/interface/PerformanceBoundGenerator.h>
+#include <traces/interface/ConvergenceGenerator.h>
+
+namespace trace
+{
+	/*! \brief A singleton used to create instances of trace generators */
+	class TraceConfiguration
+	{
+	public:
+		//! \brief configuration for the warp synchronous trace generator
+		class WarpSynchronous 
+		{
+		public:
+			//! \brief whether trace generator is enabled
+			bool enabled;
+
+			//! \brief emits dot file visualizing hot paths
+			bool emitHotPaths;
+			
+			//! \brief path to CSV collecting results
+			std::string csv;
+		};
+		
+		/*! \brief configuration properties for 
+				trace::PerformanceBoundGenerator trace generator
+		*/
+		class PerformanceBound {
+		public:
+			/*! \brief Enable the performance bound trace generator */
+			bool enabled;
+			
+			/*! \brief Protocol for memory coalescing */
+			PerformanceBoundGenerator::CoalescingProtocol protocol;
+		};
+		
+		/*! \brief configuration properties for ConvergenceGenerator */
+		class Convergence {
+		public:
+			//! \brief indicates trace generator is enabled
+			bool enabled;
+			
+			//! \brief log file to append results
+			std::string logfile;		
+			
+			//! \brief emit CFG as dot file
+			bool dot;	
+			
+			//! \brief if true, a PDF is constructed from the DOT file
+			bool render;
+		};
+
+	public:
+		static TraceConfiguration Singleton;
+
+	public:
+		//! \brief path to trace generation database
+		std::string database;
+
+		//! \brief indicates traces are in place
+		bool inPlaceTraces;
+
+		//! \brief trace::MemoryTraceGenerator
+		bool memory;
+
+		//! \brief trace::BranchTraceGenerator
+		bool branch;
+
+		//! \brief trace::SharedComputationTraceGenerator
+		bool sharedComputation;
+
+		//! \brief trace::ParallelismTraceGenerator
+		bool parallelism;
+
+		//! \brief trace::InstructionTraceGenerator
+		bool instruction;
+
+		//! \brief trace::CacheSimulator
+		bool cacheSimulator;
+
+		//! \brief warp synchronous trace generator
+		WarpSynchronous warpSynchronous;
+		
+		//! \brief performance bound generator
+		PerformanceBound performanceBound;
+		
+		//! \brief trace::ConvergenceGenerator
+		Convergence convergence;
+	
+	public:
+		TraceConfiguration();
+
+	private:
+		trace::MemoryTraceGenerator _memoryTraceGenerator;
+		trace::InstructionTraceGenerator _instructionTraceGenerator;
+		trace::SharedComputationGenerator _sharedComputationGenerator;
+		trace::BranchTraceGenerator _branchTraceGenerator;
+		trace::ParallelismTraceGenerator _parallelismTraceGenerator;
+		trace::CacheSimulator _cacheSimulator;
+		trace::WarpSynchronousGenerator _warpSynchronous;
+		trace::PerformanceBoundGenerator _performanceBound;
+		trace::ConvergenceGenerator _convergence;
+	};
+}
+
+#endif
+

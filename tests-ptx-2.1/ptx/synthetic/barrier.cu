1781f234b46eb8c6de616f544162332eca664439
Gregory Diamos <solusstultus@gmail.com>
2010-11-22 15:08:02 +0000

    DEL: Removed ptx 1.4 directory from ptx2.1 tests.
    Former-commit-id: dc2187a3294e28932e41e67caae62e1f199daa55

--- tests-ptx-2.1/ptx/synthetic/barrier.cu
+++
@@ -1,9 +0,0 @@
-
-__global__ void barrier( int* in, int* out )
-{
-	__shared__ int shared[8];
-	shared[ ( threadIdx.x + 1 ) % blockDim.x ] = in[ threadIdx.x ];
-	__syncthreads();
-	out[ threadIdx.x ] = shared[ threadIdx.x ];
-}
-

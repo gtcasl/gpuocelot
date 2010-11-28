d6c39a8e1fefda955b38d626f3b330d259844d99
Si Li <omikun@gmail.com>
2010-11-28 20:26:16 +0000

    Updated JSONEmitter in BasicBlockInstrumentor
    Former-commit-id: 78101acce8174a058d9ed2595c8015ccf1f7d599

--- branches/lynx/ocelot/analysis/implementation/BasicBlockInstrumentor.cpp
+++ branches/lynx/ocelot/analysis/implementation/BasicBlockInstrumentor.cpp
@@ -22,6 +22,8 @@
 
 #include <fstream>
 
+using namespace hydrazine;
+
 namespace analysis
 {
 
@@ -75,7 +77,63 @@ namespace analysis
     }
 
     void BasicBlockInstrumentor::emitJSON(size_t *info) {
-
+		json::Object *basicBlockStat = new json::Object;
+		json::Object *column;
+	
+		/* per thread BB execution count */
+		column = new json::Object;
+		json::Number *valueBBC;
+		basicBlockStat->dictionary["per_thread_BB_execution_count"] = column;
+		for(unsigned int i=0; i<threadBlocks; i++)
+		{
+			for(unsigned int j=0; j<threads; j++)
+			{
+				for(unsigned int k=0; k<basicBlocks; k++)
+				{
+					std::stringstream thread;
+					thread << i << "." << j << "." << k;
+					valueBBC = new json::Number(int(info[i*threads*basicBlocks+j*basicBlocks+k]));
+					column->dictionary[thread.str()] = valueBBC;
+				}
+			}
+		}
+	
+		/* per cta bbc */ 
+		column = new json::Object;
+		basicBlockStat->dictionary["per_CTA_BB_execution_count"] = column;
+		for(unsigned int i=0; i<basicBlocks; i++)
+		{
+			for(unsigned int j=0; j<threadBlocks; j++)
+			{
+				std::stringstream thread;
+				thread << j << "." << i;
+				int sum = 0;
+				for(unsigned int k=0; k<threads; k++)
+				{
+					sum += info[j*threads*basicBlocks+k*basicBlocks+i];
+				}
+				valueBBC = new json::Number(sum);
+				column->dictionary[thread.str()] = valueBBC;
+			}
+		}
+	
+		/* total bbc */ 
+		column = new json::Object;
+		basicBlockStat->dictionary["BB_execution_count"] = column;
+		for(unsigned int i=0; i<basicBlocks; i++)
+		{
+			std::stringstream thread;
+			thread << i;
+			int sum = 0;
+			for(unsigned int j=0; j<threads*threadBlocks; j++)
+			{
+				sum += info[j*basicBlocks+i];
+			}
+			valueBBC = new json::Number(sum);
+			column->dictionary[thread.str()] = valueBBC;
+		}
+		
+		jsonEmitter("basicBlockCount", basicBlockStat);
     }
 
     BasicBlockInstrumentor::BasicBlockInstrumentor() : description("Basic Block Execution Count Per Thread") {

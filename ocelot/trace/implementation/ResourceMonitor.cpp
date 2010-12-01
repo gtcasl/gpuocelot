eb32837872c28c1a469238be59b60bcf3bc9a374
Si Li <omikun@mycompany.com>
2010-12-01 18:24:21 +0000

    added missing resource monitor implementation

---
+++ ocelot/trace/implementation/ResourceMonitor.cpp
@@ -0,0 +1,97 @@
+/*! \file ResourceMonitor.cpp
+	\date Wednesday Nov 30, 2010
+	\author Si Li <sli@gatech.edu>
+	\brief The source file for the ResourceMonitor class.
+*/
+
+#ifndef RESOURCE_MONITOR_CPP_INCLUDED
+#define RESOURCE_MONITOR_CPP_INCLUDED
+
+// Ocelot includes
+#include <ocelot/trace/interface/ResourceMonitor.h>
+#include <ocelot/trace/interface/TraceEvent.h>
+#include <ocelot/executive/interface/EmulatedKernel.h>
+#include <ocelot/executive/interface/Device.h>
+
+// hydrazine includes
+#include <hydrazine/implementation/Exception.h>
+#include <iostream>
+
+namespace trace
+{
+	ResourceMonitor::Allocation::Allocation( bool v, ir::PTXU64 b, ir::PTXU64 e )
+		: valid( v ), base( b ), extent( e )
+	{
+	
+	}
+	
+//	void ResourceMonitor::_gatherGlobalAllocation()
+//	{
+//		if( _cache.base > *address 
+//			|| *address >= _cache.base + _cache.extent
+//			|| !_cache.valid )
+//		{
+//			const executive::Device::MemoryAllocation* allocation = 
+//				_device->getMemoryAllocation( (void*)*address, 
+//					executive::Device::AnyAllocation );
+//			if( allocation == 0 )
+//			{
+//				globalMemoryError( _device, _dim,
+//					thread, *address, e.memory_size, e, _kernel );
+//			}
+//			_cache.base = ( ir::PTXU64 ) allocation->pointer();
+//			_cache.extent = allocation->size();
+//			if( *address >= _cache.base + _cache.extent )
+//			{
+//				globalMemoryError( _device, _dim,
+//					thread, *address, e.memory_size, e, _kernel );
+//			}
+//		}	
+//	}
+
+	
+	ResourceMonitor::ResourceMonitor() : _cache( false ), _parameter( true ), 
+		_shared( true ), _local( true ), _constant( true )
+	{
+	}	
+	
+	void ResourceMonitor::initialize( const executive::ExecutableKernel& kernel )
+	{
+		_dim = kernel.blockDim();
+	
+		_device = kernel.device;
+		
+		_cache.valid = false;
+
+		_parameter.base = 0;
+		_parameter.extent = kernel.parameterMemorySize();
+
+		_constant.base = 0;
+		_constant.extent = kernel.constMemorySize();
+		
+		_shared.base = 0;
+		_shared.extent = kernel.totalSharedMemorySize();
+		
+		_local.base = 0;
+		_local.extent = kernel.localMemorySize();
+		
+		_kernel = static_cast< const executive::EmulatedKernel* >( &kernel );
+		unsigned int totalGlobalAllocationSize = _device->getTotalGlobalAllocation();
+		//print to proc entry
+		std::cout << "Total global memory usage: " << totalGlobalAllocationSize
+			<< "\n";
+	}
+
+	void ResourceMonitor::event(const TraceEvent& event)
+	{
+		//null
+	}
+	
+	void ResourceMonitor::finish()
+	{
+		//clear resource usage stats in proc entry
+	}
+}
+
+#endif
+

e7d89731408e5b30b4977585628fa9cfceed1136
Gregory Diamos <gregory.diamos@gatech.edu>
2011-01-30 11:14:37 +0000

    TAG: Tagged the regression test suites.

---
+++ ocelot/executive/interface/ExecutableKernel.h
@@ -0,0 +1,140 @@
+/*! \file ExecutableKernel.h
+	\author Andrew Kerr <arkerr@gatech.edu>
+	\date Jan 19, 2009
+	\brief implements a kernel that is executable on some device
+*/
+
+#ifndef EXECUTABLE_KERNEL_H_INCLUDED
+#define EXECUTABLE_KERNEL_H_INCLUDED
+
+#include <ocelot/ir/interface/Kernel.h>
+#include <ocelot/ir/interface/Texture.h>
+#include <ocelot/ir/interface/Dim3.h>
+
+namespace executive {
+	class Device;
+}
+
+namespace trace {
+	class TraceEvent;
+	class TraceGenerator;
+}
+
+namespace executive {
+	class ExecutableKernel : public ir::Kernel {
+	public:
+		typedef std::vector< trace::TraceGenerator* > TraceGeneratorVector;
+		typedef std::vector< const ir::Texture* > TextureVector;
+
+	public:
+		executive::Device* device;
+
+	public:
+		ExecutableKernel(const ir::Kernel& k, 
+			executive::Device* d = 0);
+		ExecutableKernel(executive::Device* d = 0);
+		virtual ~ExecutableKernel();
+	
+		/*!	\brief Determines whether kernel is executable */
+		virtual bool executable();
+		
+		/*!	\brief Launch a kernel on a 2D grid */
+		virtual void launchGrid(int width, int height)=0;
+
+		/*!
+			\brief compute argument offsets for argument data
+			\return number of bytes required for argument memory
+		*/
+		virtual size_t mapArgumentOffsets();
+
+		/*!
+			\brief given a block of argument memory, sets the values of 
+				each argument
+			\param argument pointer to argument memory
+			\param argumentSize number of bytes to write to argument memory
+		*/
+		virtual void setArgumentBlock(const unsigned char *argument, 
+			size_t argumentSize);
+
+		/*!
+			\brief gets the values of each argument as a block of binary data
+			\param argument pointer to argument memory
+			\param maxSize maximum number of bytes to write to argument memory
+			\return actual number of bytes required by argument memory
+		*/
+		virtual size_t getArgumentBlock(unsigned char *argument,
+			size_t maxSize) const;
+	
+		/*!	\brief Sets the shape of a kernel */
+		virtual void setKernelShape(int x, int y, int z)=0;
+
+		/*! \brief Changes the amount of external shared memory */
+		virtual void setExternSharedMemorySize(unsigned int)=0;
+		
+		/*! \brief Sets the max number of pthreads this kernel can use */
+		virtual void setWorkerThreads(unsigned int workerThreadLimit)=0;
+			
+		/*! \brief Indicate that the kernels arguments have been updated */
+		virtual void updateArgumentMemory()=0;
+		
+		/*! \brief Indicate that other memory has been updated */
+		virtual void updateMemory()=0;
+		
+		/*! \brief Get a vector of all textures references by the kernel */
+		virtual TextureVector textureReferences() const=0;
+
+		/*!	Notifies all attached TraceGenerators of an event */
+		void traceEvent(const trace::TraceEvent & event) const;
+
+		/*!	Notifies all attached TraceGenerators of completion of an event */
+		void tracePostEvent(const trace::TraceEvent & event) const;
+		
+		/*!	adds a trace generator to the EmulatedKernel */
+		virtual void addTraceGenerator(trace::TraceGenerator* generator)=0;
+
+		/*!	removes a trace generator from an EmulatedKernel */
+		virtual void removeTraceGenerator(trace::TraceGenerator* generator)=0;
+
+	public:
+		/*! attribute accessors - things every executable kernel should know */
+		unsigned int constMemorySize() const;
+		unsigned int localMemorySize() const;
+		unsigned int maxThreadsPerBlock() const;
+		unsigned int registerCount() const;
+		unsigned int sharedMemorySize() const;
+		unsigned int externSharedMemorySize() const;
+		unsigned int totalSharedMemorySize() const;
+		unsigned int argumentMemorySize() const;
+		unsigned int parameterMemorySize() const;
+		const ir::Dim3& blockDim() const;
+		const ir::Dim3& gridDim() const;
+
+	protected:
+		/*! \brief Total amount of allocated constant memory size */
+		unsigned int _constMemorySize;
+		/*! \brief Total amount of allocated local memory size */
+		unsigned int _localMemorySize;
+		/*! \brief Maxmimum number of threads launched per block */
+		unsigned int _maxThreadsPerBlock;
+		/*! \brief Number of registered required by each thread */
+		unsigned int _registerCount;
+		/*! \brief The amount of allocated static shared memory */
+		unsigned int _sharedMemorySize;
+		/*! \brief The amount of allocated dynamic shared memory */
+		unsigned int _externSharedMemorySize;
+		/*! \brief Total amount of packed parameter memory */
+		unsigned int _argumentMemorySize;
+        /*! \brief Kernel stack parameter memory space */
+        unsigned int _parameterMemorySize;
+		/*! \brief The block dimensions */
+		ir::Dim3 _blockDim;
+		/*!	\brief Dimension of grid in blocks */
+		ir::Dim3 _gridDim;
+		/*! \brief Attached trace generators */
+		TraceGeneratorVector _generators;
+
+	};
+	
+}
+
+#endif
---
+++ ocelot/ir/interface/PTXOperand.h
@@ -0,0 +1,242 @@
+/*! \file PTXOperand.h
+	\author Andrew Kerr <arkerr@gatech.edu>
+	\date Jan 15, 2009
+	\brief internal representation of a PTX operand
+*/
+
+#ifndef IR_PTXOPERAND_H_INCLUDED
+#define IR_PTXOPERAND_H_INCLUDED
+
+#include <string>
+#include <vector>
+#include <functional>
+#include <ocelot/ir/interface/Instruction.h>
+
+namespace ir {
+
+	typedef unsigned char PTXU8;
+	typedef unsigned short PTXU16;
+	typedef unsigned int PTXU32;
+	typedef unsigned long long PTXU64;
+	
+	typedef char PTXS8;
+	typedef short PTXS16;
+	typedef int PTXS32;
+	typedef long long PTXS64;
+	
+	typedef float PTXF32;
+	typedef double PTXF64;
+	
+	typedef PTXU8 PTXB8;
+	typedef PTXU16 PTXB16;
+	typedef PTXU32 PTXB32;
+	typedef PTXU64 PTXB64;
+
+	class PTXOperand {
+	public:
+		//! addressing mode of operand
+		enum AddressMode {
+			Register,			//! use as register variable
+			Indirect,			//! indirect access
+			Immediate,			//! treat as immediate value
+			Address,			//! treat as addressable variable
+			Label,				//! operand is a label
+			Special,			//! special register
+			ArgumentList,       //! treat as argument list for function call
+			FunctionName,       //! operand is a function name
+			BitBucket,			//! bit bucket register
+			Invalid
+		};
+
+		/*! Type specifiers for instructions */
+		enum DataType {
+			s8 = 0,
+			s16,
+			s32,
+			s64,
+			u8 = 4,
+			u16,
+			u32,
+			u64,
+			f16 = 8,
+			f32,
+			f64,
+			b8,
+			b16,
+			b32,
+			b64,
+			pred,
+			_,
+			TypeSpecifier_invalid
+		};
+
+		/*!	Special register names */
+		enum SpecialRegister {
+			tid,
+			ntid,
+			laneId,
+			warpId,
+			nwarpId,
+			warpSize,
+			ctaId,
+			nctaId,
+			smId,
+			nsmId,
+			gridId,
+			clock,
+			clock64,
+			lanemask_eq,
+			lanemask_le,
+			lanemask_lt,
+			lanemask_ge,
+			lanemask_gt,
+			pm0,
+			pm1,
+			pm2,
+			pm3,
+			envreg0,
+			envreg1,
+			envreg2,
+			envreg3,
+			envreg4,
+			envreg5,
+			envreg6,
+			envreg7,
+			envreg8,
+			envreg9,
+			envreg10,
+			envreg11,
+			envreg12,
+			envreg13,
+			envreg14,
+			envreg15,
+			envreg16,
+			envreg17,
+			envreg18,
+			envreg19,
+			envreg20,
+			envreg21,
+			envreg22,
+			envreg23,
+			envreg24,
+			envreg25,
+			envreg26,
+			envreg27,
+			envreg28,
+			envreg29,
+			envreg30,
+			envreg31,
+			SpecialRegister_invalid
+		};
+		
+		enum PredicateCondition {
+			Pred,		//< instruction executes if predicate is true
+			InvPred,	//< instruction executes if predicate is false
+			PT,			//< predicate is always true
+			nPT			//< predicate is always false
+		};
+	
+		enum Vec {
+			v1 = 1, 			//< scalar
+			v2 = 2,				//< vector2
+			v4 = 4				//< vector4
+		};
+		
+		enum VectorIndex {
+			iAll = 0, //! Refers to the complete vector
+			ix = 1, //! Only refers to the x index of the vector
+			iy = 2, //! Only refers to the y index of the vector
+			iz = 3, //! Only refers to the z index of the vector
+			iw = 4 //! Only refers to the w index of the vector
+		};
+
+		typedef std::vector<PTXOperand> Array;
+
+		typedef Instruction::RegisterType RegisterType;
+
+	public:
+		static std::string toString(VectorIndex);
+		static std::string toString(DataType);
+		static std::string toString(SpecialRegister);
+		static std::string toString(AddressMode);
+		static std::string toString(DataType, RegisterType);
+		static bool isFloat(DataType);
+		static bool isInt(DataType);
+		static bool isSigned(DataType);
+		static unsigned int bytes(DataType);
+		static bool valid(DataType, DataType);
+		static bool relaxedValid(DataType instructionType, DataType);
+		static long long unsigned int maxInt(DataType type);
+		static long long unsigned int minInt(DataType type);
+		
+	public:
+		PTXOperand();
+		PTXOperand(SpecialRegister r, VectorIndex i = iAll, DataType t = u32);
+		PTXOperand(const std::string& label);
+		PTXOperand(AddressMode m, DataType t, RegisterType r = 0, 
+			int o = 0, Vec v = v1);
+		PTXOperand(AddressMode m, DataType t, const std::string& identifier, 
+			int o = 0, Vec v = v1);
+		PTXOperand(AddressMode m, const std::string& identifier);
+		~PTXOperand();
+
+		std::string toString() const;
+		std::string registerName() const;
+		unsigned int bytes() const;
+
+		//! identifier of operand
+		std::string identifier;
+		
+		//! addressing mode of operand
+		AddressMode addressMode;
+
+		//! data type for PTX instruction
+		DataType type;
+
+		//!	offset when used with an indirect addressing mode
+		union {
+			int offset;
+			VectorIndex vIndex;
+			unsigned int registerCount;
+		};
+		
+		//! immediate-mode value of operand
+		union {
+			long long unsigned int imm_uint;
+			long long int imm_int;
+			double imm_float;
+			PredicateCondition condition;
+			SpecialRegister special;
+			unsigned int localMemorySize;
+		};
+
+		union {
+			/*! Identifier for register */
+			RegisterType reg;
+			bool isArgument;
+			unsigned int sharedMemorySize;
+		};
+		
+		union {
+			//! Indicates whether target or source is a vector or scalar
+			Vec vec;
+			unsigned int stackMemorySize;
+		};
+		
+		//! Array if this is a vector
+		Array array;
+		
+	};
+
+}
+
+namespace std {
+	template<> inline size_t hash<ir::PTXOperand::DataType>::operator()( 
+		ir::PTXOperand::DataType t) const
+	{
+		return (size_t)t;
+	}
+}
+
+#endif
+

a8fa5638aab4d57e065443e589b9773e8e644ebf
Andrew Kerr <arkerr@mycompany.com>
2011-02-09 18:40:18 +0000

    ENH: implemented JSON serialization for util::ExtractedDeviceState and brief test process
    Former-commit-id: 7738e0c5915d99ea8a6fe1b82a6f5c7096110ce7

--- ocelot/util/implementation/ExtractedDeviceState.cpp
+++ ocelot/util/implementation/ExtractedDeviceState.cpp
@@ -5,22 +5,55 @@
 	\brief Data structure describing device state with serialization and deserialization procedures
 */
 
+// C++ includes
+#include <iomanip>
+
+// Ocelot includes
 #include <ocelot/util/interface/ExtractedDeviceState.h>
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
-static void emitEscapedString(std::ostream &out, std::string str) {
-	
+static void emitEscapedString(std::ostream &out, const std::string &str) {
+	for (std::string::const_iterator c_it = str.begin(); c_it != str.end(); ++c_it) {
+		if (*c_it == '"') {
+			out << "\\\"";
+		}
+		else {
+			out << *c_it;
+		}
+	}
 }
 
 static void serialize(std::ostream &out, const ir::Dim3 &dim) {
 	out << "[" << dim.x << ", " << dim.y << ", " << dim.z << "]";
 }
 
+static void serializeBinary(std::ostream &out, const size_t size, const char *bytes) {
+	const size_t wordSize = 4;
+	out << "{ \"bytes\": " << std::setbase(10) << size << ", \"image\": [\n" << std::setbase(16);
+
+	for (size_t n = 0; n < size; n += wordSize) {
+		unsigned int word = 0;
+		for (size_t j = 0; j < wordSize; j++) {
+			if (j+n < size) {
+				word |= ((unsigned int)bytes[j+n] << (8*j));		
+			}
+		}
+		if (!(n % 16)) {
+			out << "\n";
+		}
+		out << ((n) ? ", " : "") << "0x" << word;
+	}
+	out << std::setbase(10) << "] }";
+}
+
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
 util::ExtractedDeviceState::MemoryAllocation::MemoryAllocation(size_t _size): base(0), size(_size) {
 	base = new char[size];
+}
+util::ExtractedDeviceState::MemoryAllocation::MemoryAllocation(): base(0), size(0) {
+
 }
 
 util::ExtractedDeviceState::MemoryAllocation::~MemoryAllocation() {
@@ -30,15 +63,23 @@ util::ExtractedDeviceState::MemoryAllocation::~MemoryAllocation() {
 	base = 0; size = 0;
 }
 
+void util::ExtractedDeviceState::MemoryAllocation::serialize(std::ostream &out) const {
+	out << "{\"base\": " << std::setbase(16) << (const void *)base << ",\n";
+	out << "\"data\": ";
+	::serializeBinary(out, size, (const char *)base);
+	out << "}";	
+}
+
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
-void util::ExtractedDeviceState::KernelLaunch::serialize(std::ostream &out) {
+void util::ExtractedDeviceState::KernelLaunch::serialize(std::ostream &out) const {
 	out << "{ \"module\": \"" << moduleName << "\", \"kernel\": \"" << kernelName << "\",\n";
-	out << "  \"gridDim\": "; serialize(gridDim); out << ",\n";
-	out << "  \"blockDim\": "; serialize(blockDim); out << ",\n";
+	out << "  \"gridDim\": "; ::serialize(out, gridDim); out << ",\n";
+	out << "  \"blockDim\": "; ::serialize(out, blockDim); out << ",\n";
 	out << "  \"parameterMemorySize\": " << parameterMemorySize << ",\n";
 	out << "  \"sharedMemorySize\": " << sharedMemorySize << ",\n";
 	out << "  \"parameterMemory\": ";
+	serializeBinary(out, parameterMemorySize, parameterMemory);
 	out << "}";
 }
 
@@ -65,7 +106,7 @@ void util::ExtractedDeviceState::Module::clear() {
 	globalVariablesAfter.clear();
 }
 
-void util::ExtractedDeviceState::serialize(std::ostream &out) {
+void util::ExtractedDeviceState::Module::serialize(std::ostream &out) const {
 	out << "{\n";
 	out << "  \"name\": \"" << name << "\",\n";
 	out << "  \"ptx\": \"";
@@ -74,23 +115,33 @@ void util::ExtractedDeviceState::serialize(std::ostream &out) {
 	
 	out << "  \"globalsBefore\": {\n";
 	int n = 0;
-	for (GlobalVariableMap::iterator v_it = globalVariablesBefore.begin(); 
+	for (GlobalVariableMap::const_iterator v_it = globalVariablesBefore.begin(); 
 		v_it != globalVariablesBefore.end(); ++v_it ) {
 		if (!n++) { out << ",\n"; }
 		out << "    \"" << v_it->first << "\": ";
-		v_it->second.serialize(out);
+		v_it->second->serialize(out);
 	}
 	out << "},\n";
 	out << "  \"globalsAfter\": {\n";
 	n = 0;
-	for (GlobalVariableMap::iterator v_it = globalVariablesAfter.begin(); 
+	for (GlobalVariableMap::const_iterator v_it = globalVariablesAfter.begin(); 
 		v_it != globalVariablesAfter.end(); ++v_it ) {
 		
 		out << "    \"" << v_it->first << "\": ";
-		v_it->second.serialize(out);
+		v_it->second->serialize(out);
 	}
 	out << "}\n";
 	out << "}\n";
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+void util::ExtractedDeviceState::Application::serialize(std::ostream &out) const {
+	out << "{\n\"name\": \"";
+	emitEscapedString(out, cudaDevice);
+	out << "\",\n\"cudaDevice\":\""; 
+	emitEscapedString(out, cudaDevice);
+	out << "\"}";
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
@@ -105,23 +156,51 @@ util::ExtractedDeviceState::ExtractedDeviceState() {
 }
 
 util::ExtractedDeviceState::~ExtractedDeviceState() {
-	clear();
+	
 }
 
 //! \brief store data in host memory to file
-void util::ExtractedDeviceState::serialize(std::ostream &out) {
+void util::ExtractedDeviceState::serialize(std::ostream &out) const {
 	// only serialize the module containing the executed kernel
-	ModuleMap::const_iterator mod_it = modules.find(kernelLaunch.moduleName);
-	if (mod_it == modules.end()) {
-		// all modules then, and no 'after' state
-	}
-	else {
+	ModuleMap::const_iterator mod_it = modules.find(launch.moduleName);
+
 		// serialize this module, before state, kernel launch, and after state
+		size_t n = 0;
 		
 		out << "{\n";
+		out << "\"application\":";
 		
+		application.serialize(out);
+		
+		out << ", \"allocationsBefore\": [";
+		n = 0;
+		for (GlobalAllocationMap::const_iterator alloc_it = globalAllocationsBefore.begin(); 
+			alloc_it != globalAllocationsBefore.end(); ++alloc_it) {
+		
+			out << (n++ ? ",":"");
+			alloc_it->second->serialize(out);
+		}
+		
+		out << "],\n \"allocationsAfter\": [";
+		n = 0;
+		for (GlobalAllocationMap::const_iterator alloc_it = globalAllocationsBefore.begin(); 
+			alloc_it != globalAllocationsBefore.end(); ++alloc_it) {
+		
+			out << (n++ ? ",":"");
+			alloc_it->second->serialize(out);
+		}		
+		out << "],\n \"kernelLaunch\": ";
+		
+		out << "\"modules\": [";
+		n = 0;
+		for (ModuleMap::const_iterator mod_it = modules.begin(); 
+			mod_it != modules.end(); ++mod_it) {
+			
+			out << (n++ ? ",":"");
+			mod_it->second.serialize(out);
+		}
+		out << "]\n";
 		out << "}\n";
-	}
 }
 
 //! \brief load data from JSON file to host memory
--- ocelot/util/implementation/KernelTestHarness.cpp
+++ ocelot/util/implementation/KernelTestHarness.cpp
@@ -10,6 +10,7 @@
 
 // C++ includes
 #include <iostream>
+#include <cstring>
 
 // Hydrazine includes
 #include <hydrazine/implementation/ArgumentParser.h>
@@ -19,28 +20,159 @@
 
 // CUDA Includes
 
+// Ocelot includes
+#include <ocelot/util/interface/ExtractedDeviceState.h>
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
+static void testExtractedDeviceStateSerialize() {
+	util::ExtractedDeviceState deviceState;
+	
+	// setup device state
+	deviceState.application.name = "KernelTestHarness";
+	deviceState.application.cudaDevice = "simulated CUDA device";
+	
+	// allocations before
+	util::ExtractedDeviceState::MemoryAllocation alloc[6];
+	alloc[0].base = new char[128];
+	alloc[0].size = 128;
+	for (size_t n = 0; n < alloc[0].size; n++) {
+		alloc[0].base[n] = (char )(n >> 1);
+	}
+	alloc[1].base = new char[32];
+	alloc[1].size = 32;
+	for (size_t n = 0; n < alloc[1].size; n++) {
+		alloc[1].base[n] = (char )n;
+	}
+	alloc[2].base = new char[32];
+	alloc[2].size = 32;
+	for (size_t n = 0; n < alloc[2].size; n++) {
+		alloc[2].base[n] = 0;
+	}
+	
+	// allocations after
+	alloc[3].base = new char[128];
+	alloc[3].size = 128;
+	for (size_t n = 0; n < alloc[3].size; n++) {
+		alloc[3].base[n] = (char )(n >> 1);
+	}
+	alloc[4].base = new char[32];
+	alloc[4].size = 32;
+	for (size_t n = 0; n < alloc[4].size; n++) {
+		alloc[4].base[n] = (char )n;
+	}
+	alloc[5].base = new char[32];
+	alloc[5].size = 32;
+	for (size_t n = 0; n < alloc[5].size; n++) {
+		alloc[5].base[n] = (alloc[3].base[n] & alloc[4].base[n]);
+	}
+	deviceState.globalAllocationsBefore[alloc[0].base] = &alloc[0];
+	deviceState.globalAllocationsBefore[alloc[1].base] = &alloc[1];
+	deviceState.globalAllocationsBefore[alloc[2].base] = &alloc[2];
+	deviceState.globalAllocationsAfter[alloc[3].base] = &alloc[3];
+	deviceState.globalAllocationsAfter[alloc[4].base] = &alloc[4];
+	deviceState.globalAllocationsAfter[alloc[5].base] = &alloc[5];
+	
+	// modules
+	util::ExtractedDeviceState::Module module;
+	module.name = "testModule";
+	module.ptx = "	.version 2.1"
+"	.target sm_20"
+".entry genericmemory ("
+".param .u64 __cudaparm_genericmemory_A)"
+"{"
+".reg .u32 %r<9>;"
+".reg .u64 %rd<5>;"
+".reg .pred %p<9>;"
+".shared .u32 $rs;"
+".local .u32 $rl;"
+"$BB_01:"
+""
+"	ld.param.u64 %rd1, [__cudaparm_genericmemory_A];"
+"	"
+"	mov.u64 %rd2, %rd1;"
+"	mov.u64 %rd3, $rs;"
+"	mov.u64 %rd4, $rl;"
+"	"
+"	//"
+"	//"
+"	//"
+"	"
+"	cvta.global.u64 %rd2, %rd2;"
+"	cvta.shared.u64 %rd3, %rd3;"
+"	cvta.local.u64 %rd4, %rd4;"
+"	"
+"	//"
+"	//"
+"	//"
+"	"
+"	isspacep.global %p0, %rd2;			// expect TRUE"
+"	isspacep.shared %p1, %rd3;			// expect TRUE"
+"	isspacep.local %p2, %rd4;				// expect TRUE"
+""
+"	isspacep.global %p3, %rd3;			// expect all others to be FALSE"
+"	isspacep.shared %p4, %rd4;"
+"	isspacep.local %p5, %rd2;"
+""
+"	isspacep.global %p6, %rd4;"
+"	isspacep.shared %p7, %rd2;"
+"	isspacep.local %p8, %rd3;"
+"	"
+"	//"
+"	//"
+"	//"
+"	"
+"	selp.u32 %r0, 1000, 0, %p0;"
+"	selp.u32 %r8, 1008, 0, %p8;"
+"	"
+""
+"	st.global.u32 [%rd1+0], %r0;"
+"	st.global.u32 [%rd1+4], %r1;"
+"	st.global.u32 [%rd1+8], %r2;"
+"	"
+"$BB_02:"
+"	exit;"
+"}"
+"";
+	
+	deviceState.modules[module.name] = module;
+	
+	// kernel launch
+	deviceState.launch.moduleName = "";
+	deviceState.launch.kernelName = "";
+	deviceState.launch.blockDim = ir::Dim3(32, 2, 1);
+	deviceState.launch.gridDim = ir::Dim3(4, 4, 4);
+	deviceState.launch.parameterMemorySize = 24;
+	deviceState.launch.sharedMemorySize = 0;
+	deviceState.launch.parameterMemory = new char[deviceState.launch.parameterMemorySize];
+	std::memcpy(&deviceState.launch.parameterMemory[0], &alloc[0].base, sizeof(void *));
+	std::memcpy(&deviceState.launch.parameterMemory[8], &alloc[1].base, sizeof(void *));
+	std::memcpy(&deviceState.launch.parameterMemory[16], &alloc[2].base, sizeof(void *));
+	
+	// serialize device state
+	deviceState.serialize(std::cout);
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char *argv[]) {
-	hydrazine::ArgumentParser parser;
+	hydrazine::ArgumentParser parser(argc, argv);
 	
 	std::string input;
 	bool help;
+	bool testSerialization;
 	
 	parser.parse("-i", "--input", input, "", "Serialization output from libKernelExtractor.la");
 	parser.parse("-h", "--help", help, false, "Displays this usage statement.");
+	parser.parse("-s", "--test-serialization", testSerialization, false, "Performs a sample serialization to stdout");
 	
-	
-	
-	if (help || input == "") {
-	
+	std::cout << "KernelTestHarness:\n";
+		testExtractedDeviceStateSerialize();
+		
+	if (help) {
+
 	}
-	else {
-	
+	else if (testSerialization) {
 	}
 
 	return 0;

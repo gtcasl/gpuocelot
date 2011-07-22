be67a9e235dfd42e4819ab6329e70e05950b0eea
Naila Farooqui <gtnaila@mycompany.com>
2011-07-22 16:49:35 +0000

    BUG: Fixed branch divergence code specification to only look for predicated branch instructions.

--- resources/branchDivergence.c
+++ resources/branchDivergence.c
@@ -1,6 +1,7 @@
 {
     ON_INSTRUCTION:
     PREDICATE: 
+    BRANCH:
     {
         unsigned long offset = warpCount() * instructionId() + warpId();
         deviceMem[offset] = predicateEvalWarpDivergent();

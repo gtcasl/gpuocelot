0e250b8e77cda720ad5dcb9cd468c41d0ba646b1
Gregory Diamos <solusstultus@gmail.com>
2011-09-14 18:29:55 +0000

    ENH: Added default install path option to build.py.
    Former-commit-id: 88314edf13fa7700cb9847628b32fe27ff35dde1

--- ocelot/build.py
+++ ocelot/build.py
@@ -125,6 +125,11 @@ def submit(options, testPassed):
 ################################################################################
 ## Main
 def main():
+	defaultInstallPath = '/usr/local'
+	
+	if 'OCELOT_INSTALL_PATH' in os.environ:
+		defaultInstallPath = os.environ['OCELOT_INSTALL_PATH']
+
 	parser = OptionParser()
 	
 	parser.add_option( "-c", "--clean", \
@@ -144,7 +149,7 @@ def main():
 	parser.add_option( "-w", "--no_werr", \
 		default = False, action = "store_true", help = "don't turn warnings into errors." )
 	parser.add_option( "-p", "--install_prefix", \
-		default = "/usr/local", help = "The base path to install ocelot in." )
+		default = defaultInstallPath, help = "The base path to install ocelot in." )
 	parser.add_option( "--build_deb", \
 		default = False, action = "store_true",
 		help = "Build a .deb package of Ocelot." )

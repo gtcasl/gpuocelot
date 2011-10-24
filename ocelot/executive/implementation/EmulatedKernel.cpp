526d202844d9d28c96fa8bcfa7b5d38dab22f18b
Gregory Diamos <gregory.diamos@gatech.edu>
2011-10-24 08:41:14 +0000

    ENH: Consistent tab formatting in emulatedkernel.
    Former-commit-id: fdd752783ca6d3ef2a7fc3ae842b9b71e5d9ceb4

--- ocelot/executive/implementation/EmulatedKernel.cpp
+++ ocelot/executive/implementation/EmulatedKernel.cpp
@@ -278,44 +278,46 @@ void executive::EmulatedKernel::constructInstructionSequence() {
 	Data movement instructions: ld, st
 */
 void executive::EmulatedKernel::updateParamReferences() {
-  using namespace std;
-  for (PTXInstructionVector::iterator 
-    i_it = instructions.begin();
-    i_it != instructions.end(); ++i_it) {
-    ir::PTXInstruction& instr = *i_it;
-    if (instr.addressSpace == ir::PTXInstruction::Param) {
-      if (instr.opcode == ir::PTXInstruction::Ld 
-        && instr.a.addressMode == ir::PTXOperand::Address) {
-        
-        ir::Parameter *pParam = getParameter(instr.a.identifier);
-        if (pParam) {
-          ir::Parameter &param = *pParam;
-          instr.a.isArgument = param.isArgument() && !function();
-          report("For instruction '" << instr.toString() 
-              << "' setting source parameter '" << instr.a.toString() 
-              << "' offset to " << param.offset << " " 
-              << ( instr.a.isArgument ? "(argument)" : "" ) );
-          instr.a.offset += param.offset;
-          instr.a.imm_uint = 0;
+	using namespace std;
+	for (PTXInstructionVector::iterator 
+		i_it = instructions.begin();
+		i_it != instructions.end(); ++i_it) {
+		ir::PTXInstruction& instr = *i_it;
+		if (instr.addressSpace == ir::PTXInstruction::Param) {
+			if (instr.opcode == ir::PTXInstruction::Ld 
+				&& instr.a.addressMode == ir::PTXOperand::Address) {
+				
+				ir::Parameter *pParam = getParameter(instr.a.identifier);
+				if (pParam) {
+					ir::Parameter &param = *pParam;
+					instr.a.isArgument = param.isArgument() && !function();
+					report("For instruction '" << instr.toString() 
+							<< "' setting source parameter '"
+							<< instr.a.toString() 
+							<< "' offset to " << param.offset << " " 
+							<< ( instr.a.isArgument ? "(argument)" : "" ) );
+					instr.a.offset += param.offset;
+					instr.a.imm_uint = 0;
 				}
-		  }
-		  else if (instr.opcode == ir::PTXInstruction::St
-        && instr.d.addressMode == ir::PTXOperand::Address) {
-        ir::Parameter *pParam = getParameter(instr.d.identifier);
-        if (pParam) {
-          ir::Parameter &param = *pParam;
-          instr.d.isArgument = param.isArgument() && !function();
-          report("For instruction '" << instr.toString() 
-              << "' setting destination parameter '" << instr.d.toString() 
-              << "' offset to " << param.offset << " " 
-              << ( instr.d.isArgument ? "(argument)" : "" ) );
+			}
+			else if (instr.opcode == ir::PTXInstruction::St
+				&& instr.d.addressMode == ir::PTXOperand::Address) {
+				ir::Parameter *pParam = getParameter(instr.d.identifier);
+				if (pParam) {
+					ir::Parameter &param = *pParam;
+					instr.d.isArgument = param.isArgument() && !function();
+					report("For instruction '" << instr.toString() 
+							<< "' setting destination parameter '"
+							<< instr.d.toString() 
+							<< "' offset to " << param.offset << " " 
+							<< ( instr.d.isArgument ? "(argument)" : "" ) );
 
-          instr.d.offset += param.offset;
-          instr.d.imm_uint = 0;
+					instr.d.offset += param.offset;
+					instr.d.imm_uint = 0;
 				}
-      }
-    }
-  }
+			}
+		}
+	}
 }
 
 void executive::EmulatedKernel::initializeArgumentMemory() {

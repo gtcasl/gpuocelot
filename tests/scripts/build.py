6b9bde13a4c21a109d94a59eaa2ec934c3310c3a
Andrew Kerr <arkerr@gmail.com>
2011-12-22 22:59:08 +0000

    Demonstrated CUDA 2.2 SDK tests with  Scons-based build system
    Former-commit-id: 90e335f659b43b60ad94dbd81964b8c797dae06c

--- tests/scripts/build.py
+++ tests/scripts/build.py
@@ -47,16 +47,15 @@ def InitializeEnvironment(env, builder, paths = ['-I./sdk',]):
 	# get CUDA paths
 	(cuda_exe_path, cuda_lib_path, cuda_inc_path)  = getCudaPaths()
 	
-	print "cuda_inc_path = ", cuda_inc_path
-	
 	env.Append(CPPFLAGS=['-Wall', '-O2', '-I.', '-I' + cuda_inc_path, '-std=c++0x'] + paths)
 
 	nvccPath = cuda_exe_path + ('/' if cuda_exe_path != '' else '')
 	for shaderModel in ('10', '11', '12', '13', '20'):
+		options = "-arch=sm_20"
+		print "nvcc", options
 		env.Append(BUILDERS = {'Cuda' + ('SM' + shaderModel if shaderModel != '20' else ''): builder(
-			action= nvccPath + 'nvcc -I. -I' + cuda_inc_path + ' -I./sdk -arch=compute_' + shaderModel \
-				+ ' -code=sm_20 $SOURCE -c -o $TARGET',
-			suffix = '.o',
+			action = nvccPath + 'nvcc -I. -I' + cuda_inc_path + ' -I./sdk ' + options + ' $SOURCE -cuda -o $TARGET',
+			suffix = '.cu.cpp',
 			src_suffix = '.cu'
 		)})
 	return env
@@ -67,18 +66,10 @@ def InitializeEnvironment(env, builder, paths = ['-I./sdk',]):
 def MapSource(env, source):
 	if len(source) > 8:
 		suffix = source[-8:].lower()
-		builder = {'_sm10.cu': env.CudaSM10, '_sm11.cu': env.CudaSM11, '_sm12.cu': env.CudaSM12, '_sm13.cu': env.CudaSM13 }
+		builder = {'_sm10.cu': env.CudaSM10, '_sm11.cu': env.CudaSM11, '_sm12.cu': env.CudaSM12, 
+			'_sm13.cu': env.CudaSM13 }
 		if suffix in builder.keys():
 			return (builder[suffix])(source)
-			
-#	if len(source) > 8 and source[-8:].lower() == '_sm10.cu':
-#		return env.CudaSM10(source)
-#	if len(source) > 8 and source[-8:].lower() == '_sm11.cu':
-#		return env.CudaSM11(source)
-#	if len(source) > 8 and source[-8:].lower() == '_sm12.cu':
-#		return env.CudaSM12(source)
-#	if len(source) > 8 and source[-8:].lower() == '_sm13.cu':
-#		return env.CudaSM13(source)
 	if source[-3:] == ".cu":
 		return env.Cuda(source)
 	return source

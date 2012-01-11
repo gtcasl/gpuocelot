ff0ba1356536b84cf55575a4f0d56ef223cde6b9
Andrew Kerr <arkerr@gmail.com>
2012-01-11 22:03:37 +0000

    ENH: added SConstruct build system for cuda3.2 SDK applications
    Former-commit-id: e928f66c9ee344e875dd2a323476565d1479e7cb

--- tests/scripts/build.py
+++ tests/scripts/build.py
@@ -51,10 +51,10 @@ def InitializeEnvironment(env, builder, paths = ['-I./sdk',]):
 
 	nvccPath = cuda_exe_path + ('/' if cuda_exe_path != '' else '')
 	for shaderModel in ('10', '11', '12', '13', '20'):
-		options = "-arch=sm_20"
+		options = "-arch=sm_20 $SPECIAL_OPTION"
 		print "nvcc", options
 		env.Append(BUILDERS = {'Cuda' + ('SM' + shaderModel if shaderModel != '20' else ''): builder(
-			action = nvccPath + 'nvcc -I. -I' + cuda_inc_path + ' -I./sdk ' + options + ' $SOURCE -cuda -o $TARGET',
+			action = nvccPath + 'nvcc -I. -I' + cuda_inc_path + ' -I./sdk -I./shared  ' + options + ' $SOURCE -cuda -o $TARGET',
 			suffix = '.cu.cpp',
 			src_suffix = '.cu'
 		)})
@@ -63,7 +63,7 @@ def InitializeEnvironment(env, builder, paths = ['-I./sdk',]):
 # 
 # Applies build rules to CUDA sources
 #
-def MapSource(env, source):
+def MapSource(env, source, options=''):
 	if len(source) > 8:
 		suffix = source[-8:].lower()
 		builder = {'_sm10.cu': env.CudaSM10, '_sm11.cu': env.CudaSM11, '_sm12.cu': env.CudaSM12, 
@@ -71,6 +71,6 @@ def MapSource(env, source):
 		if suffix in builder.keys():
 			return (builder[suffix])(source)
 	if source[-3:] == ".cu":
-		return env.Cuda(source)
+		return env.Cuda(source, SPECIAL_OPTION=options)
 	return source
 

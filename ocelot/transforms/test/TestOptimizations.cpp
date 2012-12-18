85da83db134287063010a24c1a0d4108583189ec
Gregory Diamos <gregory.diamos@gatech.edu>
2012-12-18 19:48:47 +0000

    BUG: Alias analysis should treat kernel arguments differently from parameters used in the body.
    Former-commit-id: c53e4b1e154b82a4b78a574f1bbfaa1d3d66fba6

--- ocelot/transforms/test/TestOptimizations.cpp
+++ ocelot/transforms/test/TestOptimizations.cpp
@@ -226,7 +226,7 @@ int main(int argc, char** argv)
 		"Search path for PTX files." );
 	parser.parse( "-O", "--optimizations", test.optimizations, 
 		"dead-code-elimination, loop-unrolling, function-inlining, "
-		"global-value-numbering, constant-propagation", 
+		"global-value-numbering, constant-propagation, hoist-parameters", 
 		"Comma separated list of optimizations to run." );
 	parser.parse();
 
